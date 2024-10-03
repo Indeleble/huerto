bcebb20796fd4a8f91fdcf126337c82fd6ee6253
guillermo.casas <guillermo.casas@econocom.com>
2024-10-03 12:15:58 +0200

    DropDownMenu

--- app/src/main/java/com/wyllyw/huertoplan/screens/FirstScreen.kt
+++ app/src/main/java/com/wyllyw/huertoplan/screens/FirstScreen.kt
@@ -4,21 +4,100 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.wyllyw.huertoplan.navigation.AppScreens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FirstScreen(navController: NavController) {
 
-    Scaffold {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var menuExpanded by remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Centered Top App Bar", maxLines = 1, overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { menuExpanded = !menuExpanded  }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                        DropdownMenu(
+                            expanded = menuExpanded,
+                            onDismissRequest = { menuExpanded = false },
+                        ) {
+                            // 6
+                            DropdownMenuItem(
+                                text = {
+                                    Text("Refresh")
+                                },
+                                onClick = { /* TODO */ },
+                            )
+                            DropdownMenuItem(
+                                text = {
+                                    Text("Settings")
+                                },
+                                onClick = { /* TODO */ },
+                            )
+                            DropdownMenuItem(
+                                text = {
+                                    Text("About")
+                                },
+                                onClick = { /* TODO */ },
+                            )
+                        }
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) {
         BodyContent(navController)
     }
 
@@ -33,7 +112,7 @@ fun BodyContent(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Primera pantalla")
-        Button(onClick = {navController.navigate(AppScreens.SecondScreen.route)}) {
+        Button(onClick = { navController.navigate(AppScreens.SecondScreen.route) }) {
             Text("Boton")
         }
     }
