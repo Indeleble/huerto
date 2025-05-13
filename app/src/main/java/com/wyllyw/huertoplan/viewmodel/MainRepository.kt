f92d59d9c0a3380186729251f4f5b51bf9f7a48e
guillermo.casas <guillermo.casas@econocom.com>
2025-05-13 14:56:52 +0200

    Añadida funcionalidad del repo con plantas

--- app/src/main/java/com/wyllyw/huertoplan/viewmodel/MainRepository.kt
+++ app/src/main/java/com/wyllyw/huertoplan/viewmodel/MainRepository.kt
@@ -2,6 +2,8 @@ package com.wyllyw.huertoplan.viewmodel
 
 import android.content.Context
 import com.wyllyw.huertoplan.model.Bancal
+import com.wyllyw.huertoplan.model.FamiliaPlanta
+import com.wyllyw.huertoplan.model.PlantaHorticola
 import com.wyllyw.huertoplan.model.Sector
 import com.wyllyw.huertoplan.model.Terrain
 import com.wyllyw.huertoplan.model.User
@@ -15,7 +17,7 @@ import javax.inject.Singleton
  * how to make an instance of this class
  */
 @Singleton
-class MainRepository @Inject constructor(context: Context) {
+class MainRepository @Inject constructor(private val context: Context) {
 
     /**
      * Mocking internet call, in real case this function will fetch a new integer
@@ -28,53 +30,83 @@ class MainRepository @Inject constructor(context: Context) {
     }
 
     fun getUser(name: String): User {
-
         return User(
-            name, arrayListOf(
+            name,
+            arrayListOf(
                 Terrain(
-                    "Terreno uno", "Asturias", arrayListOf(
+                    "Terreno uno", "Asturias",
+                    arrayListOf(
                         Sector(
-                            "Sector 1", arrayListOf(
-                                Bancal("Bancal 1",
+                            "Sector 1",
+                            arrayListOf(
+                                Bancal(
+                                    "Bancal 1",
                                     id = 1,
                                     x = 0f,
                                     y = 0f,
                                     width = 1f,
                                     height = 5f
-                                )),
-                              //  Bancal("Bancal 2"),
-                              //  Bancal("Bancal 3")
-                            )
-                        ),
-                       /* Sector(
-                            "Sector 2", arrayListOf(
-                              //  Bancal("Bancal 1"),
-                              //  Bancal("Bancal 2"),
-                              //  Bancal("Bancal 3")
-                            )
-                        )*/
-                    )
-                ),
-               /* Terrain(
-                    "Terreno dos", "Salamanca", arrayListOf(
-                        Sector(
-                            "Sector 1", arrayListOf(
-                                Bancal("Bancal 1"),
-                                Bancal("Bancal 2"),
-                                Bancal("Bancal 3")
-                            )
-                        ),
-                        Sector(
-                            "Sector 2", arrayListOf(
-                                Bancal("Bancal 1"),
-                                Bancal("Bancal 2"),
-                                Bancal("Bancal 3")
-                            )
+                                )
+                            ),
                         )
+                    ),
+                )
+            ),
+        )
+    }
+
+    fun getFamilias(): List<FamiliaPlanta> {
+        val familias = mutableListOf<FamiliaPlanta>()
+        context.assets.open("familias_plantas.csv").bufferedReader().use { reader ->
+            // Saltar la línea de encabezados
+            reader.readLine()
+
+            reader.forEachLine { line ->
+                val (id, nombre, descripcion) = line.split(",")
+                familias.add(
+                    FamiliaPlanta(
+                        id = id.toInt(),
+                        nombre = nombre,
+                        descripcion = descripcion
                     )
-                )*/
-            )
+                )
+            }
+        }
+        return familias
+    }
 
+    fun getPlantas(): List<PlantaHorticola> {
+        val plantas = mutableListOf<PlantaHorticola>()
+        context.assets.open("plantas_horticolas.csv").bufferedReader().use { reader ->
+            // Saltar la línea de encabezados
+            reader.readLine()
 
+            reader.forEachLine { line ->
+                val partes = line.split(",")
+                plantas.add(
+                    PlantaHorticola(
+                        id = partes[0].toInt(),
+                        nombreComun = partes[1],
+                        variedad = if (partes[2].isBlank()) null else partes[2],
+                        familiaId = partes[3].toInt(),
+                        nombreCientifico = partes[4],
+                        tiempoRecoleccion = partes[5].toInt()
+                    )
+                )
+            }
+        }
+        return plantas
+    }
+
+    fun getPlantasPorFamilia(familiaId: Int): List<PlantaHorticola> {
+        return getPlantas().filter { it.familiaId == familiaId }
+    }
+
+    fun getFamiliaPorId(familiaId: Int): FamiliaPlanta? {
+        return getFamilias().find { it.id == familiaId }
+    }
+
+    fun addUser(user: User) {
+        // Implementación pendiente
     }
 }
